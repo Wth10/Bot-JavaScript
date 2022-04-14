0d5143c87a8c06157ffe742bc765f81fc5f85584
Wth10 <weuberth10@gmail.com>
2022-04-14 01:23:53 -0300

    UpdateFormat

--- http.js
+++ http.js
@@ -4,8 +4,8 @@ const readlineSync = require('readline-sync');
 async function Bot() {
   const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
-  const Money1 = readlineSync.question("Base Inform One Currency: ");
-  const Money2 = readlineSync.question("Inform a Final Coin: ");
+  const Money1 = readlineSync.question('Base Inform One Currency: ');
+  const Money2 = readlineSync.question('Inform a Final Coin: ');
 
   const Url = 'https://www.google.com/search?q=+'+Money1+'+para+'+Money2+'&oq='+Money1+'+para+'+Money2 +'&aqs=chrome..69i57j69i60.5928j0j4&sourceid=chrome&ie=UTF-8';
   await page.goto(Url);
